b69cc2ea8f9312f19e427d3014969893507f707d
Yael <slinkyloverhere@gmail.com>
2015-03-08 13:52:19 +0100

    Update main_eficiencia.m

--- main_eficiencia.m
+++ main_eficiencia.m
@@ -1,35 +1,20 @@
-matlabpool open local 8
+matlabpool open local 8 % abrimos sesión para la paralelización el número indica el número de workers
 
 lambda = [30:0.001:90];
 indice = [30:0.001:90] * 0 + 4;
 radio = 7.5;
 orden = 10;
 tiempo=0;
-tic;
+tic; % empieza a contar el tiempo de ejecucion del codigo paralelizado
 
+% dividimos las lambdas entre los workers para su paralelización
 parfor i = 1:length(lambda)
     
     [Qsca(i), Qext(i), Qabs(i)] = eficiencia_worker(lambda(i), indice(i), orden, radio);
     
 end
-
-% figure;
-% plot(lambda,Qsca,'r', 'LineWidth', 2)
-% hold on
-% plot(lambda,Qext,'b', 'LineWidth', 2)
-% plot(lambda,Qabs,'g', 'LineWidth', 2)
-% hold off
-% legend({'$Q_{sca}$','$Q_{ext}$','$Q_{abs}$'}, 'Interpreter','latex','Location','best')
-% legend boxoff
-% box on
-% set(gca,'FontSize',20,'FontName','Helvetica')
-% xlabel('$Wavelength / nm$','Interpreter','latex', 'FontSize',24)
-% ylabel('$Q$','Interpreter','latex', 'FontSize',24)
-% title('\emph{  sphere radius = 30 nm}', 'Interpreter','latex')
-% print -dpng Q_sph_30nm.png
-% hold off
-tiempo=toc
-save('resultados.mat')
+tiempo=toc; % acaba de contar el tiempo de ejecucion del codigo paralelizado
+save('resultados.mat'); %guarda el workspace en el directorio actual
 
 matlabpool close;
 exit;
